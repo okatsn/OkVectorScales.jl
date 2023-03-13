d77a039b4c96c3283669332abbdecfd16ce507a2
Documenter.jl <documenter@juliadocs.github.io>
2023-03-13 08:13:47 +0000

    build based on e3cac98

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkVectorScales","category":"page"},{"location":"#OkVectorScales","page":"Home","title":"OkVectorScales","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkVectorScales.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkVectorScales]","category":"page"}]
+}
